b8e29339b082c00fad3c9f0bf326106af463844c
sqe <azizbek@gmail.com>
2023-05-17 20:19:16 -0700

    ep

--- source/cloud_metadata.py
+++ source/cloud_metadata.py
@@ -3,11 +3,20 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import json
+from functools import wraps
+from requests.exceptions import HTTPError as HTTPError
+from requests.exceptions import RequestException as RequestException
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 s = requests.Session()
+
+target_url_list = [
+
+]
+
+
 
 @app.errorhandler(500)
 def internal_server_error(error):
@@ -27,13 +36,17 @@ def home():
 
 @app.route("/cm")
 def metadata():
-    with s:
-        SSRF_url = "http://169.254.169.254/latest/meta-data/"
-        get_data_request = s.get(url=SSRF_url)
-        get_data_request.raise_for_status()
-        get_data_response = json.dumps(get_data_request.json())
+    try:
+        with s:
+            SSRF_url = "http://instance-data/latest/meta-data/"
+            get_data_request = s.get(url=SSRF_url)
+            get_data_request.raise_for_status()
+            get_data_response = json.dumps(get_data_request.json())
+            return get_data_response
+    except HTTPError as HE:
+        return HE
+    except RequestException as RE:
+        return RE
 
-        return get_data_response
-    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="80", threaded=True)
